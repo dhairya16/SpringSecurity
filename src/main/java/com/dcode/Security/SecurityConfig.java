69e69c2978d0ad056239d1bb4337e428643cd9c0
Dhairya Savlekar <dhairyasavlekar16@gmail.com>
2025-07-07 17:09:48 +0530

    jwt token genration

--- src/main/java/com/dcode/Security/SecurityConfig.java
+++ src/main/java/com/dcode/Security/SecurityConfig.java
@@ -1,19 +1,44 @@
 package com.dcode.Security;
 
+import com.dcode.Security.filter.JWTAuthenticationFilter;
+import com.dcode.Security.util.JWTUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers.matchers;
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private JWTUtil jwtUtil;
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public SecurityConfig(JWTUtil jwtUtil, UserDetailsService userDetailsService) {
+        this.jwtUtil = jwtUtil;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -21,17 +46,31 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            AuthenticationManager authenticationManager,
+            JWTUtil jwtUtil
+    ) throws Exception {
+
+        JWTAuthenticationFilter jwtAuthenticationFilter = new JWTAuthenticationFilter(
+                authenticationManager, jwtUtil
+        );
+
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/register", "/h2-console/**").permitAll() // Allow register + H2 console
                         .anyRequest().authenticated()
                 )
                 .csrf(csrf -> csrf.disable())
-                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable())) // Disable frame options for H2 console
-                .httpBasic(Customizer.withDefaults());
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable())); // Disable frame options for H2 console
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        return new ProviderManager(Arrays.asList(daoAuthenticationProvider()));
     }
 
 }
---
+++ src/main/java/com/dcode/Security/util/JWTUtil.java
@@ -0,0 +1,27 @@
+package com.dcode.Security.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+    private static final String SECRET_KEY = "my-super-secret-key-which-is-32-byte!";
+    private static final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+
+    public String generateToken(String username, long expiryMinutes) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiryMinutes * 60 * 1000)) //in milliseconds
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+}
